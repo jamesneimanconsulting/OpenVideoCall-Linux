de22f35e4e75da5dd4e4e3b91288ef5e915378ba
Letao Zhang <zhangletao@agora.io>
2018-02-04 18:51:17 -0800

    remove some event handler

--- sample/OpenVideoCall/EngineModel/AGEngineEventHandler.cpp
+++ sample/OpenVideoCall/EngineModel/AGEngineEventHandler.cpp
@@ -32,6 +32,8 @@ void AGEngineEventHandler::onRejoinChannelSuccess(const char* channel, uid_t uid
 
 void AGEngineEventHandler::onWarning(int warn, const char* msg)
 {
+    if(m_receiver != NULL)
+        m_receiver->onEvent(EID_WARNING, NULL);
 }
 
 void AGEngineEventHandler::onError(int err, const char* msg)
@@ -42,11 +44,9 @@ void AGEngineEventHandler::onError(int err, const char* msg)
 
 void AGEngineEventHandler::onAudioQuality(uid_t uid, int quality, unsigned short delay, unsigned short lost)
 {
-    if(m_receiver != NULL) m_receiver->onEvent(EID_AUDIO_QUALITY, NULL); } void AGEngineEventHandler::onAudioVolumeIndication(const AudioVolumeInfo* speakers, unsigned int speakerNumber, int totalVolume)
-{
     if(m_receiver != NULL)
-        m_receiver->onEvent(EID_AUDIO_VOLUME_INDICATION, NULL);
-}
+        m_receiver->onEvent(EID_AUDIO_QUALITY, NULL); 
+} 
 
 void AGEngineEventHandler::onLeaveChannel(const RtcStats& stat)
 {
@@ -56,48 +56,14 @@ void AGEngineEventHandler::onLeaveChannel(const RtcStats& stat)
 
 void AGEngineEventHandler::onRtcStats(const RtcStats& stat)
 {
-}
-
-void AGEngineEventHandler::onMediaEngineEvent(int evt)
-{
     if(m_receiver != NULL)
-        m_receiver->onEvent(EID_MEDIA_ENGINE_EVENT, NULL);
+        m_receiver->onEvent(EID_RTC_STAT, NULL);
 }
 
 void AGEngineEventHandler::onAudioDeviceStateChanged(const char* deviceId, int deviceType, int deviceState)
 {
     if(m_receiver != NULL)
         m_receiver->onEvent(EID_AUDIO_DEVICE_STATE_CHANGED, NULL);
-}
-
-void AGEngineEventHandler::onVideoDeviceStateChanged(const char* deviceId, int deviceType, int deviceState)
-{
-    if(m_receiver != NULL)
-        m_receiver->onEvent(EID_VIDEO_DEVICE_STATE_CHANGED, NULL);
-}
-
-void AGEngineEventHandler::onLastmileQuality(int quality)
-{
-    if(m_receiver != NULL)
-        m_receiver->onEvent(EID_LASTMILE_QUALITY, NULL);
-}
-
-void AGEngineEventHandler::onFirstLocalVideoFrame(int width, int height, int elapsed)
-{
-    if(m_receiver != NULL)
-        m_receiver->onEvent(EID_FIRST_LOCAL_VIDEO_FRAME, NULL);
-}
-
-void AGEngineEventHandler::onFirstRemoteVideoDecoded(uid_t uid, int width, int height, int elapsed)
-{
-    if(m_receiver != NULL)
-        m_receiver->onEvent(EID_FIRST_REMOTE_VIDEO_DECODED, NULL);
-}
-
-void AGEngineEventHandler::onFirstRemoteVideoFrame(uid_t uid, int width, int height, int elapsed)
-{
-    if(m_receiver != NULL)
-        m_receiver->onEvent(EID_FIRST_REMOTE_VIDEO_FRAME, NULL);
 }
 
 void AGEngineEventHandler::onUserJoined(uid_t uid, int elapsed)
@@ -124,40 +90,10 @@ void AGEngineEventHandler::onUserMuteVideo(uid_t uid, bool muted)
         m_receiver->onEvent(EID_USER_MUTE_VIDEO, NULL);
 }
 
-void AGEngineEventHandler::onStreamMessage(uid_t uid, int streamId, const char* data, size_t length)
-{
-    if (m_receiver != NULL)
-        m_receiver->onEvent(EID_STREAM_MESSAGE, NULL);
-}
-
-void AGEngineEventHandler::onApiCallExecuted(const char* api, int error)
-{
-    if (m_receiver != NULL)
-        m_receiver->onEvent(EID_APICALL_EXECUTED, NULL);
-}
-
-void AGEngineEventHandler::onLocalVideoStats(const LocalVideoStats& stats)
-{
-    if(m_receiver != NULL)
-        m_receiver->onEvent(EID_LOCAL_VIDEO_STAT, NULL);
-}
-
-void AGEngineEventHandler::onRemoteVideoStats(const RemoteVideoStats& stats)
-{
-    if(m_receiver != NULL)
-        m_receiver->onEvent(EID_REMOTE_VIDEO_STAT, NULL);
-}
-
 void AGEngineEventHandler::onCameraReady()
 {
     if(m_receiver != NULL)
         m_receiver->onEvent(EID_CAMERA_READY, NULL);
-}
-
-void AGEngineEventHandler::onVideoStopped()
-{
-    if (m_receiver != NULL)
-        m_receiver->onEvent(EID_VIDEO_STOPPED, NULL);
 }
 
 void AGEngineEventHandler::onConnectionLost()
@@ -168,27 +104,13 @@ void AGEngineEventHandler::onConnectionLost()
 
 void AGEngineEventHandler::onConnectionInterrupted()
 {
-
+    if(m_receiver != NULL)
+        m_receiver->onEvent(EID_CONNECTION_INTERRUPTED, NULL);
 }
 
 void AGEngineEventHandler::onUserEnableVideo(uid_t uid, bool enabled)
 {
+    if(m_receiver != NULL)
+        m_receiver->onEvent(EID_USER_ENABLE_VIDEO, NULL);
 }
 
-void AGEngineEventHandler::onStartRecordingService(int error)
-{
-    if (m_receiver != NULL)
-        m_receiver->onEvent(EID_START_RCDSRV, NULL);
-}
-
-void AGEngineEventHandler::onStopRecordingService(int error)
-{
-    if (m_receiver != NULL)
-        m_receiver->onEvent(EID_STOP_RCDSRV, NULL);
-}
-
-void AGEngineEventHandler::onRefreshRecordingServiceStatus(int status)
-{
-    if (m_receiver != NULL)
-        m_receiver->onEvent(EID_REFREASH_RCDSRV, NULL);
-}
